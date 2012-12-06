063a2c44f1590eb088969af9eeccfb8c86f58d80
coderaiser <mnemonic.enemy@gmail.com>
2012-12-06 04:23:04 -0500

    added files

--- vk-crypt.js
+++
@@ -1,9 +0,0 @@
-var VKCrypt;
-function(){
-  "use strict";
-
-  var password;
-      plaintext = lMsg.textContent;
-      ciphertext = Aes.Ctr.encrypt(plaintext, password, 256);
-      origtext = Aes.Ctr.decrypt(ciphertext, password, 256);
-}();
